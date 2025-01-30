90f757dd66dc865422be719e33b275e2736ef94c
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-01-31 00:22:14 +0100

    Finished task 1

---
+++ Week 1: Introduction to Python/task1.py
@@ -0,0 +1,12 @@
+# Contact information
+name: str = "martin"
+age: int = "21"
+phone_number: str = "123123131"
+email: str =  "mebd@kth.se"
+
+# Display contact information
+print("Contact Information:")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"phone number: {phone_number}")
+print(f"email {email}")
